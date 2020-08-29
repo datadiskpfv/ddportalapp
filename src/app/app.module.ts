1ba6cacce696375c3cab8c1392661802d368fb8b
datadiskpfv <paul.valle@datadisk.co.uk>
2020-08-29 11:25:00 +0100

    added a interceptor to add the authorization header and the JWT token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';    // added to handle client requests
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';    // added to handle client requests
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthenticationService} from './service/authentication.service';
+import {UserService} from './service/user.service';
+import {AuthInterceptor} from './interceptor/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, UserService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthenticationService} from '../service/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
+    // just return on the below URL's
+    if (httpRequest.url.includes(`${this.authenticationService.host}/user/login`)) {
+      return httpHandler.handle(httpRequest);
+    }
+
+    if (httpRequest.url.includes(`${this.authenticationService.host}/user/register`)) {
+      return httpHandler.handle(httpRequest);
+    }
+
+    if (httpRequest.url.includes(`${this.authenticationService.host}/user/reset-password`)) {
+      return httpHandler.handle(httpRequest);
+    }
+
+    // all other URL's we will add the authorization header and the JWT token
+    this.authenticationService.loadToken();
+    const token = this.authenticationService.getToken();
+
+    const request = httpRequest.clone({ setHeaders: {Authorization: `Bearer ${token}`}});
+    return httpHandler.handle(request);
+  }
+}
